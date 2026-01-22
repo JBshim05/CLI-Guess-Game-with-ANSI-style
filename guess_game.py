8cd0b5fab983337e80ed6f0f5f24092588eb8c02
JBshim05 <jbshim1023@gmail.com>
2026-01-22 03:00:13 -0500

    add main menu loop with additional small change of code

--- guess_game.py
+++ guess_game.py
@@ -4,6 +4,7 @@ import random
 # for example, redf is "red foreground" and redb is "red background"
 colors = {
 	"redf" : "\033[31m",
+	"greenf" : "\033[32m",
 	"whitef" : "\033[37m",
 	"redb" : "\033[41m",
 	"greenb" : "\033[42m",
@@ -19,36 +20,66 @@ maxNum = 50 # Max range of random number
 live = 5 # number of guesses
 rand = random.randint(1, maxNum)
 
-# Instruction
-print(f"{colors['greenb']}{mods['bold']}")
-print(f"============== {colors['redf']}INSTRUCTION!{colors['whitef']} ==============\nComputer will choose a number between 1-{maxNum}.\nYou have {live} chances to guess the correct number.\nEnter your guess and win the game!{mods['reset']}")
-print(f"{colors['blueb']}{mods['bold']}")
+game_title = r"""
+=======================================================
+   ___                         ___                     
+  / _ \_   _  ___  ___ ___    / _ \__ _ _ __ ___   ___ 
+ / /_\/ | | |/ _ \/ __/ __|  / /_\/ _` | '_ ` _ \ / _ \
+/ /_\\| |_| |  __/\__ \__ \ / /_\\ (_| | | | | | |  __/
+\____/ \__,_|\___||___/___/ \____/\__,_|_| |_| |_|\___|
 
-while(True):
-	user = 0
+======================================================="""
+
+game_instruction = f"""
+============== {colors['redb']}INSTRUCTION!{colors['greenb']} ==============
+Computer will choose a number between 1-{maxNum}.
+You have {live} chances to guess the correct number.
+Enter your guess and win the game!{mods['reset']}"""
+
+def game_logic():
+	global live
 	
-	# Input Handler
 	while(True):
-		try:
-			print("guess the correct number: ", end="")
-			user = int(input())
-			if(user <= 0):
-				print(f"{colors['redb']}PLEASE ENTER A NUMBER BIGGER THAN ZERO{colors['blueb']}")
-			elif(user > maxNum):
-				print(f"{colors['redb']}PLEASE ENTER A NUMBER LOWER OR EQUAL TO {maxNum}{colors['blueb']}")
-			else:
-				break
-		except ValueError:
-			print(f"{colors['redb']}PLEASE ENTER A NUMBER{colors['blueb']}")
-	
-	# Game Logic
-	if(rand == user):
-		print(f"{colors['greenb']}============== YOU WON! =============={mods['reset']}\n")
-		break
-	elif(live <= 0):
-		print(f"{colors['redb']}YOU LOST! The Answer Was {rand}{mods['reset']}\n")
-		break
-	else:
-		print(f"{live} left: {colors['redf']}WRONG!{colors['whitef']} ", end="")
-		print("UP!" if (rand > user) else "DOWN!")
-		live -= 1
+		user = 0
+		
+		# Input Handler
+		while(True):
+			try:
+				user = int(input("guess the correct number: "))
+				if(user <= 0):
+					print(f"{colors['redb']}PLEASE ENTER A NUMBER BIGGER THAN ZERO{colors['blueb']}")
+				elif(user > maxNum):
+					print(f"{colors['redb']}PLEASE ENTER A NUMBER LOWER OR EQUAL TO {maxNum}{colors['blueb']}")
+				else:
+					break
+			except ValueError:
+				print(f"{colors['redb']}PLEASE ENTER A NUMBER{colors['blueb']}")
+		
+		# Game Logic
+		if(rand == user):
+			print(f"{colors['greenb']}============== YOU WON! =============={mods['reset']}\n")
+			input("Enter to continue...")
+			break
+		elif(live <= 0):
+			print(f"{colors['redb']}YOU LOST! The Answer Was {rand}{mods['reset']}\n")
+			input("Enter to continue...")
+			break
+		else:
+			print(f"{live} left: {colors['redf']}WRONG!{colors['whitef']} ", end="")
+			print("UP!" if (rand > user) else "DOWN!")
+			live -= 1
+
+def game_menu():
+	while(True):		
+		print(f"{colors['greenf']}{mods['bold']}{game_title}{colors['whitef']}")
+		print(f"{colors['greenb']}{mods['bold']}{game_instruction}")
+		print(f"{colors['blueb']}{mods['bold']}")
+		
+		print("(1) Start\n(2) Exit")
+		user = int(input("Select the number: "))
+		if(user == 1):
+			game_logic()
+		elif(user == 2):
+			break
+
+game_menu()
